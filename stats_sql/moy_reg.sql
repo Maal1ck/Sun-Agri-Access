78519b6caa17288bf3bd09daaff0b4ee88f65119
Maal1ck <hadjmalickpro@gmail.com>
2025-12-27 02:15:57 +0100

    fixed alot of hydrology files

---
+++ stats_sql/moy_reg.sql
@@ -0,0 +1,11 @@
+SELECT 
+    regions.id,
+    regions.nom, 
+    ROUND(AVG(ouvrages."Profondeur")::numeric, 2) as prof_moy,
+    regions.geom -- On garde la forme du polygone
+FROM 
+    regions 
+JOIN 
+    ouvrages ON ST_Intersects(regions.geom, ouvrages.geom)
+GROUP BY 
+    regions.id, regions.nom, regions.geom -- On doit tout grouper
